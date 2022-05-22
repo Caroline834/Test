1781ebe2d0cddd74bae94b942446c93182f5fb2c
Caroline834 <95998531+Caroline834@users.noreply.github.com>
2022-05-22 22:48:04 +0630

    Update pic_puzzle.js

--- pic_puzzle.js
+++ pic_puzzle.js
@@ -1,11 +1,26 @@
-let imgMedium=['./nature/greenlake.jpg','./nature/hill.jpg','./nature/icemountain.jpg','./nature/lake.jpg'];
+let imgEasy =['./animal/animal2.jpg','./animal/bird.jpg','./animal/fish.jpg','./animal/puppy.jpg']
+let imgMedium = ['./nature/greenlake.jpg', './nature/hill.jpg', './nature/icemountain.jpg', './nature/lake.jpg'];
+let imgHard = ['./fruit/apple.jpg','./fruit/berry.jpg','./fruit/lime.jpg','./fruit/strawberry.jpg']
 let image1 = document.getElementById("p1");
 let image2 = document.getElementById("p2");
 let image3 = document.getElementById("p3");
 let image4 = document.getElementById("p4");
-function easyMode(){
-        image1.src = imgMedium[0];
-        image2.src = imgMedium[1];
-        image3.src = imgMedium[2];
-        image4.src = imgMedium[3];
+
+function easyMode() {
+    image1.src = imgEasy[0];
+    image2.src = imgEasy[1];
+    image3.src = imgEasy[2];
+    image4.src = imgEasy[3];
+}
+function mediumMode() {
+    image1.src = imgMedium[0];
+    image2.src = imgMedium[1];
+    image3.src = imgMedium[2];
+    image4.src = imgMedium[3];
+}
+function hardMode() {
+    image1.src = imgHard[0];
+    image2.src = imgHard[1];
+    image3.src = imgHard[2];
+    image4.src = imgHard[3];
 }
