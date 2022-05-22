e0169e4d859c7905cd3ea647510312b8400f3d2a
Caroline834 <95998531+Caroline834@users.noreply.github.com>
2022-05-22 21:30:05 +0630

    Update pic_puzzle.js

--- pic_puzzle.js
+++ pic_puzzle.js
@@ -1,9 +1,11 @@
 let imgMedium=['./nature/greenlake.jpg','./nature/hill.jpg','./nature/icemountain.jpg','./nature/lake.jpg'];
-let image = document.querySelector('img');
-
+let image1 = document.getElementById("p1");
+let image2 = document.getElementById("p2");
+let image3 = document.getElementById("p3");
+let image4 = document.getElementById("p4");
 function easyMode(){
-    for (let index = 0; index < imgMedium.length; index++) {
-        image.src = imgMedium[index];
-    }return index;
-
+        image1.src = imgMedium[0];
+        image2.src = imgMedium[1];
+        image3.src = imgMedium[2];
+        image4.src = imgMedium[3];
 }
